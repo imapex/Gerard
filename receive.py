3f0c6c72bd036547255dc4a1a1a5bf2464d96b72
Brad Bester <brbester@users.noreply.github.com>
2017-08-03 16:12:16 -0400

    Updates running webhook receiver (#11)
    * initial code and requirements
    
    * updated requirements.txt
    
    * updated requirements.txt
    
    * updated requirements.txt
    
    * updated port
    
    * changed port
    
    * add instructions, fixes
    
    * added ngrokwebhook
    
    * added ngrokwebhook and at file open
    
    * updated isntructions
    
    * gerard reflector code
    
    * gerard reflector code
    
    * gerard reflector code

---
+++ receive.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python
+
+from flask import Flask, request, abort
+import requests
+from ciscosparkapi import CiscoSparkAPI, Webhook
+
+
+#grab the at from a local at.txt file instead of global variable
+fat=open ("at.txt","r+")
+at=fat.readline().rstrip()
+fat.close
+
+api=CiscoSparkAPI(at)
+
+app=Flask(__name__)
+
+
+@app.route('/gerard', methods=['POST'])
+def gerard():
+    if request.method == 'POST':
+        webhook_obj = Webhook(request.json)
+        print(request.json)
+        
+        message = api.messages.get(webhook_obj.data.id)
+        me = api.people.me()
+        
+        if message.personId == me.id: 
+           print("bot spoke")
+           return 'Ok', 200
+        else:
+           roomid = webhook_obj.data.roomId
+           message_text = message.text
+           response_message = api.messages.create(roomid, text=message_text)
+           print (response_message)
+    else:
+        abort(400)
+    return 'Ok', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
+
